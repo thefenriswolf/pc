f10f3b4320f04d6c553e66ae0bfa79b348e8e791
thefenriswolf <stefan.rohrbacher97@gmail.com>
2022-01-10 20:02:07 +0100

    04

---
+++ 04-dynamic-python/shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.pythonFull
+    pkgs.wget
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+  shellHook = ''
+    wget https://raw.githubusercontent.com/thefenriswolf/FutureLab/main/FutureLab.ipynb
+  '';
+}
